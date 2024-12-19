888e07aeaad6ab04566eb7af947983d34846539c
cyrhades <cyrhades76@gmail.com>
2024-12-19 08:52:51 +0100

    correction erreur requete SQL

--- .docker/db/init.sql/users.sql
+++ .docker/db/init.sql/users.sql
@@ -15,6 +15,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `users` ( `username`, `email`, `password`, `githublink`, `apikey`, `roles`, `activated`,	) VALUES
+INSERT INTO `users` ( `username`, `email`, `password`, `githublink`, `apikey`, `roles`, `activated`) VALUES
 ('Admin', 'admin@devsocialnetwork.com', '$2b$10$SOm3kYv4SPbR0YWVUdy48rL6zG7u9FY5fdnDf8JWSfH/75IJjT95O', '', 'ZQLY3AI-YHTE2QI-QZ6IBUA-VGR2NYA', '[\"ADMIN\"]','1');
 COMMIT;
